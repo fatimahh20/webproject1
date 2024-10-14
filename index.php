6ea4d9272e2801378697f5828cea4cc22a421038
fatimahh20 <muteeahfatima@gmail.com>
2024-10-14 18:06:21 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+echo "<h3> PROJECT TITLE </h3> ";
+echo "<h3> PROJECT DETAILS </h3>";
+echo "<h3> GROUP MEMBERS </h3> ";
+echo "Samreen Zahra &nbsp BSCS51F22R027<br>";
+echo "Muteeah Fatima BSCS51F22R035<br>";
+echo "Aneeqa Liaqat &nbsp BSCS51F22S069<br>";
+?>
